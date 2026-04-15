7480e183244e7875aaee5aee843417bf864e64a0
kanupriya1293 <kanupriya1293@gmail.com>
2026-04-15 16:44:15 -0400

    Changed home page design and implemented universal tags

---
+++ assets/js/articles.js
@@ -0,0 +1,15 @@
+// Single source of truth for article metadata.
+// Tags here drive both the homepage cards and each article page's tag label.
+// To add a new article: push a new object to this array.
+
+const ARTICLES = [
+  {
+    num:    '01',
+    slug:   'vision-has-speed',
+    url:    'articles/vision-has-speed/',
+    title:  'Vision has a speed. And we are not the fastest.',
+    excerpt: 'Every creature inhabits its own perceptual world. The same room is experienced by each entirely differently.',
+    tags:   ['Biology', 'Neuroscience', 'Perception'],
+    date:   '14 April 2026',
+  },
+];
